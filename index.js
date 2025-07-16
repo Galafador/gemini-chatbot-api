cc76ec81e77c6e8f754de188ec7caedee08e368b
galafador <ghozi.naufal.a@gmail.com>
2025-07-17 06:46:14 +0700

    trying to move markdown and syntax highlighting logic to backend, then returns html to frontend

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { GoogleGenerativeAI } from '@google/generative-ai'
+// for proper markdown rendering and syntax highlighting
+import  { marked } from 'marked'
+import hljs from 'highlight.js'
 
 
 // loads environment variable from dotenv
@@ -29,12 +32,22 @@ const PORT = process.env.PORT || 3000
 const genAI = new GoogleGenerativeAI(API_KEY)
 const model = genAI.getGenerativeModel({ model: MODEL_NAME })
 
+// Configure marked to use highlight.js
+marked.setOptions({
+    highlight: (code, lang) => {
+        const language = lang && hljs.getLanguage(lang) ? lang : 'plaintext'
+        return hljs.highlight(code, { language }).value
+    },
+    langPrefix: 'language-' // important to match css class prefix
+})
+
 app.listen(PORT, () => {
     console.log(`Gemini Chatbot running on http://localhost:${PORT}`)
 })
 
 
 // Route chat
+/*
 app.post('/api/chat', async (req, res) => {
     const userMessage = req.body.message
 
@@ -46,10 +59,42 @@ app.post('/api/chat', async (req, res) => {
         const result = await model.generateContent(userMessage)
         const response = await result.response
         const text = response.text()
+        console.log(text)
 
         res.json({ reply: text })
     } catch (error) {
         console.error(error)
         res.status(500).json({ reply: "Something went wrong, please try again" })
+    }
+})
+*/
+
+/*
+The backend renders outputted text from Markdown to HTML with syntax highlighting, before
+sending it to the frontend
+*/
+
+
+app.post('/api/chat', async (req, res) => {
+    const userMessage = req.body.message
+
+    if (!userMessage) {
+        return res.status(400).json({ reply: "No prompt is being received by the backend." })
+    }
+
+    try {
+        const result = await model.generateContent(userMessage)
+        const response = result.response
+        const text = response.text()
+
+        //check on console
+        console.log(text)
+
+        res.json({ reply: text })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: error.message })
+
     }
 })
